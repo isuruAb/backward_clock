e9e768d18b417904a7a45883000971ef27a211f4
isuruAb <vpisabeywardana@gmail.com>
2021-05-08 12:49:45 +0800

    add basic component structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+import Clock from "./containers/Clock";
+import { ROUTES } from "./utils/constants";
 import "./utils/styles/custom.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path={ROUTES.ROOT} component={Clock} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
