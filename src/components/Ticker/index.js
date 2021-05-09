e7ef3563cff921ab0af5b74c882e6a443b7e6627
isuruAb <vpisabeywardana@gmail.com>
2021-05-09 21:52:50 +0800

    minor style changes

--- src/components/Ticker/index.js
+++ src/components/Ticker/index.js
@@ -2,16 +2,18 @@ import { memo } from "react";
 import PropTypes from "prop-types";
 import styles from "./index.module.scss";
 
-const Ticker = ({ value }) => {
+const Ticker = ({ value, label }) => {
   return (
     <div className={styles.wrapper}>
       <h1>{value}</h1>
+      <p>{label}</p>
     </div>
   );
 };
 
 Ticker.propTypes = {
   value: PropTypes.number,
+  label: PropTypes.string,
 };
 
 export default memo(Ticker);
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -5,9 +5,9 @@ import styles from "./index.module.scss";
 const Time = ({ hours, mins, seconds }) => {
   return (
     <div className={styles.wrapper}>
-      <Ticker value={hours} label="hours" />
-      <Ticker value={mins} label="minutes" />
-      <Ticker value={seconds} label="seconds" />
+      <Ticker value={hours} label="HOURS" />
+      <Ticker value={mins} label="MINUTES" />
+      <Ticker value={seconds} label="SECONDS" />
     </div>
   );
 };
